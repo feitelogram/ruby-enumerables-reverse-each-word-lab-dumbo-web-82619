6ebc76e50650ce531da2bfecc34146db24b95747
Nicholas Feitel <nicholas.feitel@gmail.com>
2019-08-12 14:27:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,5 +3,5 @@ new_array = []
 def reverse_each_word(sentence)
   new_array = sentence.split(" ")
   reversed_array = new_array.map {|n| n.reverse}
-  reversed_array.to_s
+  reversed_array.to_str
 end
